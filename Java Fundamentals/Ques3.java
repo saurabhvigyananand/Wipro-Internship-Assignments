de83472212dfed3cf5a25601ce2b0b88e232721d
Saurabh Kumar Vigyananand <61475311+saurabhvigyananand@users.noreply.github.com>
2022-06-19 21:23:47 +0530

    Ques3.java

---
+++ Java Fundamentals/Ques3.java
@@ -0,0 +1,9 @@
+class Ques3{
+    public static void main(String[] args){
+        int n1,n2,sum;
+        n1=Integer.parseInt(args[0]);
+        n2=Integer.parseInt(args[1]);
+        sum=n1+n2;
+        System.out.println("The sum of "+n1+" and "+n2+" is "+sum);
+    }
+}
