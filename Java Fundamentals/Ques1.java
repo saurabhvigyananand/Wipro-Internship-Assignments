9c59a7606ea180a54727d0b9523298e90eadf8b9
Saurabh Kumar Vigyananand <61475311+saurabhvigyananand@users.noreply.github.com>
2022-06-19 21:19:57 +0530

    Create Ques1.java

---
+++ Java Fundamentals/Ques1.java
@@ -0,0 +1,8 @@
+class Ques1{
+    public static void main(String[] args){
+        String fname=args[0];
+        String lname=args[1];
+        String insert=" Technologies";
+        System.out.println(fname+" "+ insert+" " + lname);
+    }
+}
